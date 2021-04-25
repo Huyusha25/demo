153ae4068513dd964492c18908594050532b346e
huys@bonree.com <huys@bonree.com>
2021-04-25 10:31:58 +0800

    framework

---
+++ TunnelServices.framework/Headers/TunnelServices.h
@@ -0,0 +1,19 @@
+//
+//  TunnelServices.h
+//  TunnelServices
+//
+//  Created by Bonree on 2021/2/23.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TunnelServices.
+FOUNDATION_EXPORT double TunnelServicesVersionNumber;
+
+//! Project version string for TunnelServices.
+FOUNDATION_EXPORT const unsigned char TunnelServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TunnelServices/PublicHeader.h>
+
+
